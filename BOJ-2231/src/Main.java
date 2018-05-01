d611e91ba36854bce6a29ddf780904d768cd651d
minsungryu <ryums0227@gmail.com>
2018-05-01 21:36:49 +0900

    solve BOJ-2231

---
+++ BOJ-2231/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            if (i + pointSum(i) == n) {
+                answer = i;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static int pointSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
