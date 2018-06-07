7d59bec5f54595fc2bb8994f2f78cc318d7228f4
minsungryu <ryums0227@gmail.com>
2018-06-07 20:54:55 +0900

    solve BOJ-15552

---
+++ BOJ-15552/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            while (st.hasMoreTokens()) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+            bw.write(String.valueOf(sum));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
